49ffdbd63b4ec1709cfa59c2ac615b4e9bdc86e6
Bruno Woltzenlogel Paleo <bruno.wp@gmail.com>
2020-09-15 11:35:55 -0300

    Simplify the code

--- scala-spec/src/main/scala/Main.scala
+++ scala-spec/src/main/scala/Main.scala
@@ -17,16 +17,19 @@ type Currency = String
 type Stablecoin = Currency
 type Cryptocurrency = Currency
 type Address = Int
+type N = BigDecimal
+
+def min(x: N, y: N) = if (x < y) x else y
 
 class Oracle {
-  private val m = MMap[(Currency,Currency), BigDecimal]()
+  private val m = MMap[(Currency,Currency), N]()
   
-  def conversionRate(from: Currency, to: Currency): BigDecimal = m((from, to))
+  def conversionRate(from: Currency, to: Currency): N = m((from, to))
   
-  def updateConversionRate(from: Currency, to: Currency, rate: BigDecimal) = { m((from, to)) = rate }
+  def updateConversionRate(from: Currency, to: Currency, rate: N) = { m((from, to)) = rate }
 }
 
-case class Transaction(from: Address, to: Address, amount: BigDecimal, currency: Currency)
+case class Transaction(from: Address, to: Address, amount: N, currency: Currency)
 
 // This is just a mock ledger. Therefore, adding a transaction does nothing
 class Ledger {
@@ -53,46 +56,57 @@ class Ledger {
 // to issue and sell reservecoins if the reserve ratio remains below a maximum threshold
 
 
-class MinimalBank(protected val address: Address, // bank's address 
-           protected val oracle: Oracle, // Oracle used by the bank
-           protected val ledger: Ledger, // Ledger in which the bank participates
-           fee: BigDecimal, // the fee charged by the bank when buying and selling stablecoins and reservecoins
-           minReserveRatio: BigDecimal, // the bank's minimum reserve ratio
-           protected val maxReserveRatio: BigDecimal, // the bank's maximum reserve ratio 
-           protected val base: Cryptocurrency, // the base cryptocurrency used by the bank (e.g. ERG or ADA)
-           reservecoin: Cryptocurrency, // shares issued by the bank to shareholders
-           stablecoin: Stablecoin, // the stablecoin issued and managed by the bank
-           protected val peg: Currency // the currency to which the stablecoin is pegged
-          ) {
+class MinimalBank(address: Address, // bank's address 
+                  oracle: Oracle, // Oracle used by the bank
+                  ledger: Ledger, // Ledger in which the bank participates
+                  fee: N, // the fee charged by the bank when buying and selling stablecoins and reservecoins
+                  minReserveRatio: N, // the bank's minimum reserve ratio
+                  maxReserveRatio: N, // the bank's maximum reserve ratio 
+                  base: Cryptocurrency, // the base cryptocurrency used by the bank
+                  reservecoin: Cryptocurrency, // shares issued by the bank to shareholders
+                  stablecoin: Stablecoin, // the stablecoin issued and managed by the bank
+                  peg: Currency // the currency to which the stablecoin is pegged
+                 ) {
   
-  // The bank's reserves in the base cryptocurrency (e.g. ERG or ADA)
-  protected var reserve: BigDecimal = 0
+  // Bank's State
+  private var reserves: N = 0     // The bank's reserves in the base cryptocurrency
+  private var stablecoins: N = 0  // The amount of stablecoins currently in circulation
+  private var reservecoins: N = 0 // The amount of reservecoins currently in circulation
   
-  // The amount of stablecoins currently in circulation
-  protected var stablecoins: BigDecimal = 0
+  // Auxiliary Functions
   
-  // The amount of reservecoins currently in circulation
-  protected var reservecoins: BigDecimal = 0
+  private def liabilities(r: N, s: N) = min(r, (s * oracle.conversionRate(peg, base) * (1 - fee)))
   
-  // The amount of reserves that would have to be paid if 
-  // all stablecoin holders decided to sell their stablecoins 
-  private def liabilities() = min(reserve.toDouble, (stablecoins * oracle.conversionRate(peg, base) * (1 - fee)).toDouble)
+  private def reserveRatio(r: N, s: N) = {
+    require(s != 0)
+    ((r * oracle.conversionRate(base, peg)) / s)
+  }
   
-  def reserveRatio(res: BigDecimal, sc: BigDecimal) = ((res * oracle.conversionRate(base, peg)) / sc)
+  private def isReserveRatioAcceptable(r: N, s: N): Boolean = {
+    if (s == 0) false
+    else {
+      val ratio = reserveRatio(r, s)
+      ratio <= maxReserveRatio && ratio >= minReserveRatio
+    }
+    
+  }
   
-  def buyStablecoin(amountBase: BigDecimal, buyerAddress: Address) = {
+
+  
+  
+  def buyStablecoin(amountBase: N, buyerAddress: Address) = {
     val amountStablecoin = amountBase * oracle.conversionRate(base, peg) * (1 - fee) 
-    if (reserveRatio(reserve + amountBase, stablecoins + amountStablecoin) > minReserveRatio) {
+    if (isReserveRatioAcceptable(reserves + amountBase, stablecoins + amountStablecoin)) {
       stablecoins = stablecoins + amountStablecoin // issue desired amount of stablecoins
-      reserve = reserve + amountBase
+      reserves = reserves + amountBase
       val transferStablecoinsToBuyer = Transaction(address, buyerAddress, amountStablecoin, stablecoin)
       val transferBaseToBank = Transaction(buyerAddress, address, amountBase, base)
       ledger.addTransactions(List(transferStablecoinsToBuyer, transferBaseToBank))
     }
   }
 
-  def sellStablecoin(amountStablecoin: BigDecimal, sellerAddress: Address) = {
-    val rl = reserveRatio(reserve, stablecoins)
+  def sellStablecoin(amountStablecoin: N, sellerAddress: Address) = {
+    val rl = reserveRatio(reserves, stablecoins)
     
     val amountBase = amountStablecoin * oracle.conversionRate(peg, base) * (1 - fee) * min(1, rl.toDouble)
     // the "min(1, rl)" factor covers the case when the reserves are not sufficient
@@ -100,36 +114,36 @@ class MinimalBank(protected val address: Address, // bank's address
     // in this case, the peg is gracefully and temporarily abandoned, to ensure that 
     // all stablecoin holders have the right to an equal share of the remaining reserves.
     
-    reserve = reserve - amountBase
+    reserves = reserves - amountBase
     val transferBaseToSeller = Transaction(address, sellerAddress, amountBase, base)
     val transferStablecoinsToBank = Transaction(sellerAddress, address, amountStablecoin, stablecoin)
     stablecoins = stablecoins - amountStablecoin // burn received stablecoins
     ledger.addTransactions(List(transferStablecoinsToBank, transferBaseToSeller))
   }
   
-  def buyReservecoin(amountBase: BigDecimal, buyerAddress: Address) = {
-    val r = reserve
-    val o = liabilities()
+  def buyReservecoin(amountBase: N, buyerAddress: Address) = {
+    val r = reserves
+    val o = liabilities(reserves, stablecoins)
     // amount of shares that the buyer will get, calculated so that the book value per share remains the same
     val amountShares = reservecoins * (((r + amountBase - o) / (r - o)) - 1) * (1 - fee)
     
     // FIXME: check that this doesn't bring the value of reserves above the max reserve ratio
     
-    reserve = reserve + amountBase
+    reserves = reserves + amountBase
     val transferBaseToBank = Transaction(buyerAddress, address, amountBase, base)
     reservecoins = reservecoins + amountShares
     
     ledger.addTransactions(List(transferBaseToBank))
   }
   
-  def sellReservecoin(amountShares: BigDecimal, sellerAddress: Address) = {
-    val r = reserve
-    val o = liabilities()
+  def sellReservecoin(amountShares: N, sellerAddress: Address) = {
+    val r = reserves
+    val o = liabilities(r, stablecoins)
     val amountBase = amountShares * (r - o)/reservecoins * (1 - fee)
     
     // FIXME: check that the sale doesn't bring reserves below accceptable ratio
     
-    reserve = reserve - amountBase
+    reserves = reserves - amountBase
     val transferBaseToSeller = Transaction(address, sellerAddress, amountBase, base)
     reservecoins = reservecoins - amountShares
 
